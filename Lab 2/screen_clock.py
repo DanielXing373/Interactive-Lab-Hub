cb4214219ff7ec1ff5e26f2d7f5c552e07cff650
DanielXing373 <yuchenxing373@gmail.com>
2026-09-13 10:36:35 -0400

    modified clock py

--- Lab 2/screen_clock.py
+++ Lab 2/screen_clock.py
@@ -62,10 +62,11 @@ backlight.value = True
 
 while True:
     # Draw a black filled box to clear the image.
-    draw.rectangle((0, 0, width, height), outline=0, fill=400)
+    draw.rectangle((0, 0, width, height), outline=0, fill=0)
 
     #TODO: Lab 2 part D work should be filled in here. You should be able to look in cli_clock.py and stats.py 
-
+    clock = time.strftime("%m/%d/%Y %H:%M:%S")
+    draw.text((x, top), clock, font=font, fill="#FFFFFF")
     # Display image.
     disp.image(image, rotation)
     time.sleep(1)
